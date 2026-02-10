74fe3100a060c45dabe4de71117bb0470743917a
Lawrence Obidike <obidikelawrence@yahoo.com>
2026-02-10 22:48:13 +0000

    base url fixes

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
-// import axiosInstance from "./axios";
 
-// then axiosInstance.interceptors...
+const API_BASE =
+  (import.meta.env.VITE_API_BASE_URL || "http://localhost:5001").replace(/\/$/, "");
 
 const axiosInstance = axios.create({
-	baseURL: import.meta?.env?.VITE_API_BASE_URL || "http://localhost:5001/api/",
-	withCredentials: true, // send cookies to the server
+  baseURL: `${API_BASE}/api`,
+  withCredentials: true,
 });
 
 export default axiosInstance;
